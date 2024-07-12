5560736764af656c479584da767d592be3fc4f42
javierM <165230714+hjmontenegro@users.noreply.github.com>
2024-07-12 01:10:40 -0300

    PrimeraEntregaMontenegro

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,7 +1,7 @@
 import carrito from '../assets/carrito.png'
 export const CartWidget = () => (
     <>
-        <img src={carrito} height={50} />
+        <img src={carrito} height={40} />
         <span>5</span>
     </>
 )
