c4155e377953c514b96fc314e754a3541c287c9c
javierM <165230714+hjmontenegro@users.noreply.github.com>
2024-07-12 00:16:51 -0300

    NavBar.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'*/
-import './App.css'
+import { NavBar } from './components/NavBar'
 
 function App() {
-  return (
-    <>
-      <div>
-        <h1>Hello Cat</h1>
-      </div>
-    </>
-  )
+  return <NavBar />;
 }
 
-export default App
+export default App;
