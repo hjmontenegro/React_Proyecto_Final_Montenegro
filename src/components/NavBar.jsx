4af2d7ceb8f1b8e491f1dbc059615790685c8b55
javierM <165230714+hjmontenegro@users.noreply.github.com>
2024-07-12 00:24:51 -0300

    CartWidget.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,1 +1,13 @@
-export const NavBar = () => <>NAVBAR</>;
+import { CartWidget } from "./CartWidget";
+
+export const NavBar = () => (
+    <>
+        <h6>Dogs Shop</h6>
+        <ul>
+            <li><a href="#">Buenos</a></li>
+            <li><a href="#">Malos</a></li>
+            <li><a href="#">Lindos</a></li>
+        </ul>
+        <CartWidget />
+    </>
+);
