c2055617620847d82e0cb4a4c62a44a192d933ac
javierM <165230714+hjmontenegro@users.noreply.github.com>
2024-08-02 02:10:14 -0300

    Finalizado

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -36,7 +36,7 @@ export const ItemListContainer = () => {
             <Container className="d-flex">
             {items.map((i) => (
                 <Card key={i.id} style={{ width: 300 }}>
-                    <Card.Img variant="top" src={i.img} height={200} />
+                    <Card.Img variant="top" src={i.img} width={200} />
                     <Card.Body>
                         <Card.Title>{i.name}</Card.Title>
                         <Card.Text>{i.detail}</Card.Text>
